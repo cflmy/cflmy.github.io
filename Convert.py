3a711449773b82c8b8ca2eef77fee66155849079
cflmy <pingan@cflmy.cn>
2025-08-22 22:03:14 +0800

    增加网站的icon

--- Convert.py
+++ Convert.py
@@ -28,6 +28,7 @@ def yaml_to_html(yaml_file_path, output_html_path):
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>我的书签</title>
+    <link rel="icon" href="/img/logo.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <style>
         body {{
